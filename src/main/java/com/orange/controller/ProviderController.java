48b333493391ac97bab1584fc5554a812e0b05da
khokhoni <khokhoni@gmail.com>
2021-12-22 16:55:17 +0200

    First commit

--- src/main/java/com/orange/controller/ProviderController.java
+++ src/main/java/com/orange/controller/ProviderController.java
@@ -6,12 +6,14 @@ import com.orange.service.ProviderResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 //mark class as Controller
 
 @Controller
+@RestController
 public class ProviderController {
 
     @Autowired
